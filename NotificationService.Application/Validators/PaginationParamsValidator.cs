8a350aa85bbc4c446983c25c10f35b94dd4adad0
Marat <marat.mkov@gmail.com>
2026-08-18 11:27:15 +0300

    refactor: move pagination validation messages into resx

--- NotificationService.Application/Validators/PaginationParamsValidator.cs
+++ NotificationService.Application/Validators/PaginationParamsValidator.cs
@@ -1,23 +1,25 @@
 using FluentValidation;
 using Microsoft.Extensions.Options;
+using NotificationService.Application.Resources;
 using NotificationService.Application.Settings;
 using NotificationService.Domain.Dtos.Pagination;
 
 namespace NotificationService.Application.Validators;
 
-public class PaginationParamsValidator  : AbstractValidator<PaginationParams>
+public class PaginationParamsValidator : AbstractValidator<PaginationParams>
 {
     public PaginationParamsValidator(IOptions<PaginationRules> pagination)
     {
         var maxPageSize = pagination.Value.MaxPageSize;
 
         RuleFor(x => x.Skip)
-            .NotNull().WithMessage($"'{nameof(PaginationParams.Skip)}' must be provided.")
-            .GreaterThanOrEqualTo(0).WithMessage($"'{nameof(PaginationParams.Skip)}' must be greater than or equal to 0.");
+            .NotNull().WithMessage(_ => string.Format(ErrorMessage.Required, nameof(PaginationParams.Skip)))
+            .GreaterThanOrEqualTo(0)
+            .WithMessage(_ => string.Format(ErrorMessage.InvalidMinValue, nameof(PaginationParams.Skip), 0));
 
         RuleFor(x => x.Take)
-            .NotNull().WithMessage($"'{nameof(PaginationParams.Take)}' must be provided.")
+            .NotNull().WithMessage(_ => string.Format(ErrorMessage.Required, nameof(PaginationParams.Take)))
             .InclusiveBetween(0, maxPageSize)
-            .WithMessage($"'{nameof(PaginationParams.Take)}' must be between 0 and {maxPageSize}.");
+            .WithMessage(_ => string.Format(ErrorMessage.InvalidRange, nameof(PaginationParams.Take), maxPageSize));
     }
 }
